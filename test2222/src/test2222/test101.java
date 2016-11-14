775bc50faa88f95c07d1dbcbeb5c466b6d50d0a1
student <student@DESKTOP-R1QOBBH>
2016-11-14 14:18:30 +0900

    기모리

--- test2222/src/test2222/test101.java
+++ test2222/src/test2222/test101.java
@@ -4,7 +4,8 @@ public class test101 {
 
 	public static void main(String[] args) {
 		System.out.println("AAA33dd");
-		System.out.println("제대로하자");
+		System.out.println("�젣��濡쒗븯�옄");
+		System.out.println("hey gay");
 	
 	}
 
